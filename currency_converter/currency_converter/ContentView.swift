46be2c0a0216f405fc51fa6a55caec2e66ef3f7e
Tan Xin Jie <xjtan99@gmail.com>
2026-02-21 14:06:05 +0800

    currency converter created

---
+++ currency_converter/currency_converter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  currency_converter
+//
+//  Created by Tan Xin Jie on 21/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ currency_converter/currency_converter/currency_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  currency_converterApp.swift
+//  currency_converter
+//
+//  Created by Tan Xin Jie on 21/2/26.
+//
+
+import SwiftUI
+
+@main
+struct currency_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
